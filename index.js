fa783febfbb1a6a06ecf79eab37632568eb61411
Muhammad Hammad <hammadasifkk@gmail.com>
2024-12-17 11:02:47 +0500

    1ST DATA

---
+++ index.js
@@ -0,0 +1,53 @@
+ let allQuestions = [
+    {
+        question: "CPU stands for?",
+        options: [
+            { answer: "Central Processing Unit ", iscorrect: true },
+            { answer: "Centre Protective Umbrella ", iscorrect: false },
+            { answer: "Cerial Processor Universe ", iscorrect: false },
+            { answer: "Candy Process Unreal ", iscorrect: false },
+        ]
+    },
+
+    {
+        question: "JS stands for?",
+        options: [
+            { answer: "Jerry Stripe ", iscorrect: false },
+            { answer: "Java Script ", iscorrect: true },
+            { answer: "Java Stripe", iscorrect: false },
+            { answer: "Java Son ", iscorrect: false },
+        ]
+    },
+
+    {
+        question: "XML stands for?",
+        options: [
+            { answer: "Xentral Money Lack ", iscorrect: true },
+            { answer: "eXtensible Markup Language ", iscorrect: false },
+            { answer: "Xerial Month loaded ", iscorrect: true },
+            { answer: "Xero Mday leak ", iscorrect: false },
+        ]
+    },
+
+    {
+        question: "HTML stands for?",
+        options: [
+            { answer: "Hentral Trocessing Mint Language",iscorrect: false },
+            { answer: "Hectre Trivia Mortella Language ", iscorrect: false },
+            { answer: "Herial Text Miniverse Language", iscorrect: false },
+            { answer: "Hyper Text Markup Language", iscorrect: true },
+        ]
+    },
+
+    {
+        question: "USB stands for?",
+        options: [
+            { answer: "Universal Serial Bus", iscorrect: true},
+            { answer: "Untre Srotective Bmbrella ", iscorrect: false },
+            { answer: "Urial Srocessor Bniverse ", iscorrect: false },
+            { answer: "Undy Socess Bnreal ", iscorrect: false },
+        ]
+    },
+
+]
+console.log(allQuestions)
